4f9b8deee55e29cf46b83d2c4f8cb78b21a8aa7d
Makoto Takizawa <taki@taki-mabro.local>
2022-12-21 10:50:21 +0900

    Add building EAP with container ACAP.

---
+++ object-detector-python/eap_app/preuninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+/usr/local/bin/docker image rm axisecp/acap-runtime:aarch64-containerized
+/usr/local/bin/docker image rm acap-dl-models
+/usr/local/bin/docker image rm acap4-object-detector-python
